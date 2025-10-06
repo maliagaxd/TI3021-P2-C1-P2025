e654a3ec124ccdf2870a252da94c813ce06e1255
maliagaxd <manuel.aliaga07@inacapmail.cl>
2025-10-06 00:03:47 -0300

    nose xd
    xdxdxd

--- clase01u2.py
+++ clase01u2.py
@@ -1,43 +1,26 @@
-class Cliente:
-    var_Contador = 0
-    def __init__(self,numero_pedido: int, fecha: str, total_a_pagar: float):
+class Pedido:
+    def __init__ (self, numero_pedido: int, fecha: str, total_a_pagar: float):
         self.__numero_pedido = numero_pedido
         self.__fecha = fecha
         self.__total_a_pagar = total_a_pagar
-        Cliente.var_Contador += 1
-    def procesar():
-        pass       
-    def obtenerDetalle(self):
-        return "Numero de pedido:" + str(self.__numero_pedido) + " creado con exito!" + "\nEres el pedido n°" + str(Cliente.var_Contador) + "\nCon un total a pagar de $" + str(self.__total_a_pagar)
 
-class Pedido_en_local(Cliente):
-    def __init__(self, numero_pedido, fecha, total_a_pagar, numero_de_mesa: int, cantidad_de_personas: int):
-        super().__init__(numero_pedido, fecha, total_a_pagar)
-        self.__numero_de_mesa = numero_de_mesa
-        self.__cantidad_de_personas = cantidad_de_personas
-    def procesar():
-        pass
-    def obtenerDetalle(self):
-        return super().obtenerDetalle()
+    def procesar(self):
+        return "Procesando su pedido :)"
 
-class Pedido_para_llevar(Cliente):
-    def __init__(self, numero_pedido, fecha, total_a_pagar, tiempo_estimado: int):
-        super().__init__(numero_pedido, fecha, total_a_pagar)
-        self.__tiempo_estimado = tiempo_estimado
-    def procesar():
-        pass   
-    def obtenerDetalle(self):
-        return super().obtenerDetalle()
+class PedidoEnLocal(Pedido):
+
+    def procesar(self):
+        return "Procesando su pedido en el local"
+
+class PedidoParaLlevar(Pedido):
+
+    def procesar(self):
+        return "Procesando su pedido para llevar"
+
+class PedidoADomicilio(Pedido):
+
+    def procesar(self):
+        return "Procesando su pedido a domicilio"
     
-class Pedido_a_domicilio(Cliente):
-    def __init__(self, numero_pedido, fecha, total_a_pagar, direccion: str, nombre_del_repartidor: str):
-        super().__init__(numero_pedido, fecha, total_a_pagar)
-        self.__direccion = direccion
-        self.__nombre_del_repartidor = nombre_del_repartidor
-    def procesar():
-        pass
-    def obtenerDetalle(self):
-        return super().obtenerDetalle() + "\nTu repartidor es: " + str(self.__nombre_del_repartidor) + "\nVa en direccion hacia: " + str(self.__direccion)
-
-clienteManuel = Pedido_a_domicilio(1001,"05/10/2025",1500.0,"Mi casa #123","Chamo")
-print(f"{clienteManuel.obtenerDetalle()}")
+pedido_1 = PedidoParaLlevar(1,"05/10/2025",1000.0)
+print(f"{pedido_1.procesar()}")
